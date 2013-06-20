53b4687acc47652f5b8394cfe35eead93d8ad381
Colin Gourlay <colin.j.gourlay@gmail.com>
2013-06-20 16:03:34 +1000

    Added sample server to index.js which attempts to read from data/port.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-/* Server should be started here */
+var fs = require('fs');
+var http = require('http');
+
+var config = {
+    host: '127.0.0.1',
+    port: 8080
+};
+
+try {
+    config.port = fs.readFileSync(__dirname + '/data/port', 'UTF-8').replace(/\s+$/, '');
+} catch (err) {}
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(config.port, config.host);
